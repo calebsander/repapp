fcde4261f54b355a66c3d1c8da7985330c212a96
Caleb Sander <calebsander@users.noreply.github.com>
2017-04-10 00:21:18 -0400

    Indentation fix; all now 2 spaces (#1)

--- server/models/link.js
+++ server/models/link.js
@@ -1,29 +1,29 @@
 module.exports = function(sequelize, DataTypes) {
-    const Link = sequelize.define('link', {
-        college: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-	    uuid: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false
-        },
-	    scheduledTime: DataTypes.RANGE(DataTypes.DATE),
-	    notesToCollege: DataTypes.TEXT,
-	    notesFromCollege: DataTypes.TEXT,
-	    notesFromCollegeSeen: DataTypes.BOOLEAN,
-	    lastSignedIn: DataTypes.DATE,
-    	repName: DataTypes.STRING
-    }, {
-        classMethods: {
-            associate: function(models) {
-                // adds a tierPriority field to links table
-                Link.belongsTo(models.tier)
-            }
-        }
-    })
+  const Link = sequelize.define('link', {
+    college: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    uuid: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    scheduledTime: DataTypes.RANGE(DataTypes.DATE),
+    notesToCollege: DataTypes.TEXT,
+    notesFromCollege: DataTypes.TEXT,
+    notesFromCollegeSeen: DataTypes.BOOLEAN,
+    lastSignedIn: DataTypes.DATE,
+    repName: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // adds a tierPriority field to links table
+        Link.belongsTo(models.tier)
+      }
+    }
+  })
 
-    return Link
+  return Link
 }
