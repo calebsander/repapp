c630ca37b97f4514e32a0aa74daeaf43a932e05a
Alex Lew <alexl.mail@gmail.com>
2017-04-10 00:11:03 -0400

    Add link and tier models

---
+++ server/models/tier.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+  const Tier = sequelize.define('tier', {
+    // Lower is more important. Colleges with lower tiers
+    // have access to more dates; dates with lower tiers
+    // let fewer colleges in. College tier must be <= date tier
+    // to sign up.
+    priority: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    // e.g. "Unavailable", "Available if necessary", "Available"
+    // In link UI, we can ask: "Allow rep to sign up for slots labeled: ..."
+    description: DataTypes.STRING
+  })
+  
+  // Finally, return the model
+  return Tier
+}
