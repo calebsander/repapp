ef93c075e2fd2aefb01d6a3397bce19a138055ac
Caleb Sander <calebsander@users.noreply.github.com>
2017-04-11 11:22:58 -0400

    Rewrite of model loading (#3)

---
+++ server/database.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const env = process.env.NODE_ENV || 'development'
+const config = require('./config/config')[env]
+const sequelize = new Sequelize(config.uri)
+
+const db = {sequelize, Sequelize}
+const modelsToAssociate = new Set
+const modelsDirectory = path.join(__dirname, 'models')
+const modelFiles = fs.readdirSync(modelsDirectory)
+for (const file of modelFiles) {
+  const model = sequelize.import(path.join(modelsDirectory, file))
+  db[model.name] = model
+  if (model.associate) modelsToAssociate.add(model)
+}
+for (const model of modelsToAssociate) model.associate(db)
+
+module.exports = db
