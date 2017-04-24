53de3fc4650a635be8c09ab81cd1cda7dd8b5c81
Alex Lew <alexl.mail@gmail.com>
2017-04-24 11:01:03 -0400

    Make API routing more modular

---
+++ server/app-routers/admin.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const adminLinksRouter = require('./admin-links')
+const adminSettingsRouter = require('./admin-settings')
+const adminUnavailabilitiesRouter = require('./admin-unavailabilities')
+
+const router = express.Router()
+router.use(adminLinksRouter)
+router.use(adminSettingsRouter)
+router.use(adminUnavailabilitiesRouter)
+
+module.exports = router
