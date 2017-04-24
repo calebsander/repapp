2659fd2a05f993c96a707c8052a81e617d0f4c46
Alex Lew <alexl.mail@gmail.com>
2017-04-24 10:25:30 -0400

    Remove login test code

--- server/app-routers/api.js
+++ server/app-routers/api.js
@@ -5,10 +5,5 @@ const loginRouter = require('./login')
 
 const router = express.Router()
 router.use('/login', loginRouter)
-router.get(
-  '/restricted',
-  restrictToLoggedIn,
-  (req, res) => res.json({success: true, response: "You're in!"})
-)
 
 module.exports = router
